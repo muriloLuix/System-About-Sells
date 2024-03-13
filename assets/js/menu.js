58dac296ecaa759b6914e4a49f26f9425f501fec
Murilo Luiz Jaboinski <143234115+muriloLuix@users.noreply.github.com>
2024-03-13 14:06:11 -0300

    Responsividade da tela inicial

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,6 +2,7 @@ const btnAbrirMenu = document.querySelector(".btn-abrir-menu");
 const btnFecharMenu = document.querySelector(".btn-fechar");
 const menuMobile = document.querySelector(".menu-mobile");
 const overlayMenu = document.querySelector(".overlay-menu");
+const menuItems = document.querySelectorAll(".menu-mobile nav ul li a");
 
 btnAbrirMenu.addEventListener("click", function () {
     menuMobile.classList.add("abrir-menu");
@@ -16,4 +17,11 @@ btnFecharMenu.addEventListener("click", function () {
 overlayMenu.addEventListener("click", function () {
     menuMobile.classList.remove("abrir-menu");
     overlayMenu.style.display = "none";
+});
+
+menuItems.forEach(item => {
+    item.addEventListener("click", function() {
+        menuMobile.classList.remove("abrir-menu");
+        overlayMenu.style.display = "none";
+    });
 });
