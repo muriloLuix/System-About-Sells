72995aede752a1b6cfaa62ad6d6552d07510a13c
Murilo Luiz Jaboinski <143234115+muriloLuix@users.noreply.github.com>
2024-03-20 17:39:04 -0300

    Adicionando Fundo nos Planos

---
+++ bg.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById("background-video");
+    var content = document.querySelector(".content");
+  
+    video.addEventListener("canplay", function() {
+        
+      video.classList.add("blur");
+
+      content.classList.add("visible");
+    });
+  });
+  
