280945cbe129c70ffebddfe7051868af700338ed
Murilo Luiz Jaboinski <muriloluiz654@gmail.com>
2024-03-21 09:52:33 -0300

    Testando responsividade

--- assets/js/screen.js
+++ assets/js/screen.js
@@ -1,5 +1,8 @@
-window.addEventListener("resize", function () {
-  if (window.innerWidth <= 1130) {
+function checkWindowSize() {
+  if (window.innerWidth <= 1000) {
     alert("Para conhecer melhor sobre o plano. Por gentileza, clique sobre o nome do plano.");
   }
-});
+}
+
+window.addEventListener("load", checkWindowSize); 
+window.addEventListener("resize", checkWindowSize); 
