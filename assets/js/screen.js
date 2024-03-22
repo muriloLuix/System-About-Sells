326e1f3b5aecf28a8eff27daa580f6a7f56e5b9b
Murilo Luiz Jaboinski <muriloluiz654@gmail.com>
2024-03-22 01:01:05 -0300

    Responsividade Planos

--- assets/js/screen.js
+++ assets/js/screen.js
@@ -1,8 +1,8 @@
-function checkWindowSize() {
-  if (window.innerWidth <= 1000) {
-    alert("Para conhecer melhor sobre o plano. Por gentileza, clique sobre o nome do plano.");
-  }
-}
+// function checkWindowSize() {
+//   if (window.innerWidth <= 1000) {
+//     alert("Para conhecer melhor sobre o plano. Por gentileza, clique sobre o nome do plano.");
+//   }
+// }
 
-window.addEventListener("load", checkWindowSize); 
-window.addEventListener("resize", checkWindowSize); 
+// window.addEventListener("load", checkWindowSize); 
+// window.addEventListener("resize", checkWindowSize); 
