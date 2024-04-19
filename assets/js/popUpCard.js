873e5ababf72c6a3158f14443279052957ef8bd0
Murilo Luiz Jaboinski <143234115+muriloLuix@users.noreply.github.com>
2024-04-19 16:39:11 -0300

    Testando alerta no no celular

--- assets/js/popUpCard.js
+++ assets/js/popUpCard.js
@@ -1,12 +1,11 @@
 function togglePopup(popupId) {
-    let popup = document.getElementById(popupId);
-    let overlay = document.getElementById("overlay");
-    if (popup.style.display === "block") {
-      popup.style.display = "none";
-      overlay.style.display = "none";
-    } else {
-      popup.style.display = "block";
-      overlay.style.display = "block";
-    }
+  let popup = document.getElementById(popupId);
+  let overlay = document.getElementById("overlay");
+  if (popup.style.display === "block") {
+    popup.style.display = "none";
+    overlay.style.display = "none";
+  } else {
+    popup.style.display = "block";
+    overlay.style.display = "block";
   }
-  
+}
