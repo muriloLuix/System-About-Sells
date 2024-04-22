e157c3098f56c94d0a5c3804d59423f22e561ba2
Murilo Luiz Jaboinski <143234115+muriloLuix@users.noreply.github.com>
2024-04-22 12:07:20 -0300

    Ajustando botões

--- assets/js/popUpCard.js
+++ assets/js/popUpCard.js
@@ -2,7 +2,7 @@ function togglePopup(popupId, overlayId) {
   let popup = document.getElementById(popupId);
   let overlay = document.getElementById(overlayId);
   if (popup && overlay) {
-    if (popup.style.display === "block") {
+    if (popup.style.display === "block" || popup.style.display === "") {
       popup.style.display = "none";
       overlay.style.display = "none";
     } else {
